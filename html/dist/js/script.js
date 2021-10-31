630cda2fede3fc3eaaef786626b025f7e75c92a6
Vladislav <vladislavdegtyarenko@gmail.com>
2021-10-31 21:54:28 +0200

    defer iframes

--- html/dist/js/script.js
+++ html/dist/js/script.js
@@ -1,14 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-   const testimonialsSlider = new Swiper('#testimonialsSlider', {
+document.addEventListener("DOMContentLoaded", function () {
+   const testimonialsSlider = new Swiper("#testimonialsSlider", {
       loop: true,
-      direction: 'horizontal',
+      direction: "horizontal",
       updateOnWindowResize: true,
       navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+         nextEl: ".swiper-button-next",
+         prevEl: ".swiper-button-prev",
       },
       pagination: {
-         el: '.swiper-pagination',
+         el: ".swiper-pagination",
       },
       slidesPerView: 3,
       breakpoints: {
@@ -19,84 +19,108 @@ document.addEventListener('DOMContentLoaded', function() {
             slidesPerView: 2,
          },
          320: {
-            slidesPerView: 1
-         }
-      }
-
-
+            slidesPerView: 1,
+         },
+      },
    });
 
-   const workSlider = new Swiper('#workSlider', {
+   const workSlider = new Swiper("#workSlider", {
       loop: false,
       slidesPerView: 3,
-      direction: 'horizontal',
+      direction: "horizontal",
       //updateOnWindowResize: true,
       pagination: {
-         el: '.swiper-pagination',
+         el: ".swiper-pagination",
       },
       breakpoints: {
          1200: {
-            slidesPerView: 3
+            slidesPerView: 3,
          },
          576: {
-            slidesPerView: 'auto'
+            slidesPerView: "auto",
          },
          320: {
-            slidesPerView: 'auto'
-         }
+            slidesPerView: "auto",
+         },
       },
    });
 
    AOS.init({
-      once: true
+      once: true,
    });
 
-   const collapseFooterMenus = document.querySelectorAll('.collapse');
-   collapseFooterMenus.forEach(hiddenContent => {
-      hiddenContent.addEventListener('show.bs.collapse', () => {
-         hiddenContent.previousElementSibling.classList.add('footer__links_collapse-active');
-      })
-      hiddenContent.addEventListener('hide.bs.collapse', () => {
-         hiddenContent.previousElementSibling.classList.remove('footer__links_collapse-active');
+   const collapseFooterMenus = document.querySelectorAll(".collapse");
+   collapseFooterMenus.forEach((hiddenContent) => {
+      hiddenContent.addEventListener("show.bs.collapse", () => {
+         hiddenContent.previousElementSibling.classList.add("footer__links_collapse-active");
+      });
+      hiddenContent.addEventListener("hide.bs.collapse", () => {
+         hiddenContent.previousElementSibling.classList.remove("footer__links_collapse-active");
+      });
+   });
 
-      })
-   })
+   const hamburgerMenu = document.querySelector(".header__hamburger");
+   const navMenu = document.querySelector(".nav");
 
+   hamburgerMenu.addEventListener("click", function () {
+      this.classList.toggle("header__hamburger-active");
+      navMenu.classList.toggle("nav-active");
+      document.body.classList.toggle("noScroll");
+   });
 
-   const hamburgerMenu = document.querySelector('.header__hamburger');
-   const navMenu = document.querySelector('.nav');
+   jsCloseHamburgerMenu = document.querySelectorAll(".jsCloseHamburgerMenu");
+   jsCloseHamburgerMenu.forEach((menuLink) => {
+      menuLink.addEventListener("click", () => {
+         navMenu.classList.remove("nav-active");
+         hamburgerMenu.classList.remove("header__hamburger-active");
+         document.body.classList.remove("noScroll");
+      });
+   });
 
-   hamburgerMenu.addEventListener('click', function() {
-      this.classList.toggle('header__hamburger-active')
-      navMenu.classList.toggle('nav-active')
-      document.body.classList.toggle('noScroll')
-   })
+   function initIframes() {
+      const iframes = document.getElementsByTagName("iframe");
+      for (let i = 0; i < iframes.length; i++) {
+         if (iframes[i].getAttribute("data-src")) {
+            iframes[i].setAttribute("src", iframes[i].getAttribute("data-src"));
+         }
+      }
+   }
+   initIframes();
 
-   jsCloseHamburgerMenu = document.querySelectorAll('.jsCloseHamburgerMenu');
-   jsCloseHamburgerMenu.forEach(menuLink => {
-      menuLink.addEventListener('click', () => {
-         navMenu.classList.remove('nav-active');
-         hamburgerMenu.classList.remove('header__hamburger-active');
-         document.body.classList.remove('noScroll');
-      })
-   })
-   
-   const howToModal = document.querySelectorAll('.modalVideo');
+   const howToModal = document.querySelectorAll(".modalVideo");
    let howToModalVideo = [];
    let howToModalVideoUrl = [];
 
    howToModal.forEach((modal, index) => {
-      howToModalVideo.push(modal.querySelector('.modal-video'))
-      howToModalVideoUrl.push(modal.querySelector('.modal-video').getAttribute('src'))
+      /*       howToModalVideo.push(modal.querySelector(".modal-video"));
+      howToModalVideoUrl.push(modal.querySelector(".modal-video").getAttribute("src")); */
 
-      modal.addEventListener('shown.bs.modal', () => {
-         if (howToModalVideo[index].getAttribute('src') !== howToModalVideoUrl[index]) {
-            howToModalVideo[index].setAttribute('src', howToModalVideoUrl[index])
-         }
-      })
-   
-      modal.addEventListener('hide.bs.modal', () => {
-         howToModalVideo[index].setAttribute('src', '')
-      })
-   })
-})
+      modal.addEventListener("shown.bs.modal", () => {
+         /*          if (howToModalVideo[index].getAttribute("src") !== howToModalVideoUrl[index]) {
+            howToModalVideo[index].setAttribute("src", howToModalVideoUrl[index]);
+         } */
+      });
+
+      modal.addEventListener("hide.bs.modal", () => {
+         modal.setAttribute("src", "");
+      });
+   });
+
+   /*    const howToVideoPlayBtn = document.querySelectorAll(".how__video_play");
+
+   howToVideoPlayBtn.forEach((btn, index) => {
+      btn.onclick = () => {
+         console.log(modal);
+         console.log(index);
+      };
+   }); */
+
+   /*    const videoModals = document.querySelectorAll(".modalVideo");
+
+   videoModals.forEach((modal, index) => {
+      modal.onclick = () => {
+         console.log(modal);
+         console.log(index);
+      };
+   }); */
+});
